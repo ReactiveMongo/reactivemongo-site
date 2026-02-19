5b28220c842a43e1e9e8f4c73a9717ed47bbd267
Cédric Chantepie <cchantep@users.noreply.github.com>
2026-02-19 16:07:59 +0100

    Try to fix CI

--- .ci_scripts/validate.sh
+++ .ci_scripts/validate.sh
@@ -12,6 +12,8 @@ for D in `(ls -v -1 "$HOME/.local/share/gem/ruby" || true) | sort -r | head -n 1
   export GEM_PATH="$HOME/.local/share/gem/ruby/$D:$GEM_PATH"
 done
 
+export GEM_PATH="$HOME/.rubygems:$GEM_PATH"
+
 if [ "x$SBT_JAR" = "x" ]; then
   SBT_JAR="$HOME/.sbt/launchers/$SBT_VER/sbt-launch.jar"
 fi
